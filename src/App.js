ee36771cf052e31ba5c3bf62b3b3988de2744aed
hertbex <mlive654@gmail.com>
2021-12-16 18:28:50 +0500

    working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import {BrowserRouter as Router, Route, Routes, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Profile from "./components/Profile/Profile";
 import Messages from "./components/Messages/Messages";
-import React from "react";
-import {useRouteMatch} from "react-router-dom";
-
 
 function App() {
-    let match = useRouteMatch();
     return (
         <Router>
             <div className={"container"}>
                 <Header/>
                 <Navbar/>
                 <main>
-                    <Switch>
-                        <Route path={"*"} component={<Profile/>}/>
-                        <Route path={`${match.path}/:dialogId`}>
-                        <Messages/>
-                        </Route>
-                        <Route path={"/profile"} component={<Profile/>}/>
-                    </Switch>
+                    <Routes>
+                        <Route path={"*"} element={<Profile/>}/>
+                        <Route path={"/messages"} element={<Messages/>}/>
+                        <Route path={"/profile"} element={<Profile/>}/>
+                    </Routes>
                 </main>
             </div>
         </Router>
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -2,17 +2,13 @@ import Chatlist from '../Chatlist/Chatlist'
 import './Messages.css'
 import state from "../../state.js"
 import Dialog from '../Dialog/Dialog'
-import React from "react";
-import {useParams} from "react-router-dom";
-
 let chats = state.chats
 let dialogs = state.dialogs
 export default function Messages(){
-    let { dialogId } = useParams();
     return(
         <div className={"messages"}>
         <div><Chatlist chats={chats}/></div> 
-        <div><Dialog messages={dialogs[dialogId]}/></div>
+        <div><Dialog messages={dialogs[0]}/></div>
         </div>
     )
 }
