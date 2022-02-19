c683ccf2424250a3117dae8d7b12c94a966ebc64
Rules <Rules>
2022-02-19 23:59:41 +0200

    draggable Chatlist https://www.youtube.com/watch?v=Qroj1wpWlp8

--- src/components/Chatlist/Chatlist.jsx
+++ src/components/Chatlist/Chatlist.jsx
@@ -1,8 +1,37 @@
 import Message from '../Message/Message'
 import {NavLink} from 'react-router-dom';
 import './Chatlist.css'
-import PullRelease from "../../hoc/PullRelease";
+import {useState} from "react";
+import DraggableListItem from "../DraggableListItem/DraggableListItem";
 export default function Chatlist(props){
+    const [chats, setChats] = useState(props.chats);
+    const [dragStartIndex, setdragStartIndex] = useState(null);
+    // get index of draged item
+    const onDragStart = (index) => setdragStartIndex(index)
+    // update list when item dropped
+    const onDrop = (dropIndex) => {
+        // get draged item
+        const dragItem = chats[dragStartIndex]
+
+        // delete draged item in list
+        let list = [...chats]
+        list.splice(dragStartIndex, 1)
+
+        // update list
+        if (dragStartIndex < dropIndex) {
+            setChats([
+                ...list.slice(0, dropIndex - 1),
+                dragItem,
+                ...list.slice(dropIndex - 1, list.length)
+            ])
+        } else {
+            setChats([
+                ...list.slice(0, dropIndex),
+                dragItem,
+                ...list.slice(dropIndex, list.length)
+            ])
+        }
+    }
     return(
         <div className={"Chatlist"}>
             {/* <div className={"Chat"}>
@@ -14,13 +43,29 @@ export default function Chatlist(props){
             <div className={"Chat"}>
                 <Message name={props.chats[2].name} text={props.chats[3].text}/>
             </div> */}
-            {props.chats.map( (chat)=>{
+            {chats.map( (chat,index)=>{
                 return(
-                    <PullRelease key={chat.id}><NavLink to={"/messages/" + chat.id} >
-                        <Message name={chat.name} text={chat.text}/>
-                    </NavLink></PullRelease>
+                    <DraggableListItem
+                        key={chat.id}
+                        index={index}
+                        onDragStart={(index) => onDragStart(index)}
+                        onDrop={(index) => onDrop(index)}
+                    >
+                        {
+                            <NavLink to={"/messages/" + chat.id} >
+                                <Message name={chat.name} text={chat.text}/>
+                            </NavLink>
+                        }
+                    </DraggableListItem>
+                    
                 )
             } )}
+            <DraggableListItem
+                key={chats.length}
+                index={chats.length}
+                draggale={false}
+                onDrop={(index) => onDrop(index)}
+            />
         </div>
     )
 }
